d757d21faf5449ca26d04cc3cf9c78821caf7bfb
ShinkaiTomomichi <yahharogim@gmail.com>
2022-07-11 00:39:03 +0900

    add Project

---
+++ SuiseiSong/SuiseiSong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuiseiSong
+//
+//  Created by shinkaitomomichi on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // とりあえずYoutubeを表示できるような面を作成
+}
+
